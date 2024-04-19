99e589987e9633865f904c0117f504d1dc111556
soundaryaBE <142423089+soundaryaBE@users.noreply.github.com>
2024-04-19 10:37:16 +0530

    Update AddDoctorTest.java

--- src/test/java/TestNG/AddDoctorTest.java
+++ src/test/java/TestNG/AddDoctorTest.java
@@ -31,7 +31,7 @@ import com.healthCare.hospiceHealthCare.objectRepository.HMSPage;
 public class AddDoctorTest extends BaseClass {
 	@Test(groups = "regression")
 	public  void AddDoctorTest() throws EncryptedDocumentException, IOException   {
-				  
+		//hii		  
 		  String SPECL = eUtil.readDataFromExcel("admin", 3, 1);
 		  String DOCNAME = eUtil.readDataFromExcel("admin", 4, 1);
 		  String DOCCLIADD = eUtil.readDataFromExcel("admin", 5, 1);
